889ecfd89a4554969afe3f1c062436c83be7ec8f
Marcin Żyżyński <mzyzynsk@mion.elka.pw.edu.pl>
2019-02-20 01:16:39 +0100

    add files

---
+++ src/main/java/com/example/pik/PikApplication.java
@@ -0,0 +1,13 @@
+package com.example.pik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikApplication.class, args);
+	}
+
+}
